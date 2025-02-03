e6a395896bec672bbf47e89f2af2db6ac9735e0b
Aman Chauhan <amanchauhan@Amans-MacBook-Pro.local>
2025-02-04 01:08:50 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://challenge-tracker-production-5443.up.railway.app";
 
 function App() {
   const [email, setEmail] = useState("test@example.com");
